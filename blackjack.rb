460a4f4fe3dd0a69416d12ca316b480e516544c2
Manuel Castillo <mancast@gmail.com>
2015-02-05 19:20:38 -0600

    Added value (10) for K, Q, J. Working on A.

--- blackjack.rb
+++ blackjack.rb
@@ -7,27 +7,73 @@ name = gets.chomp
 puts
 
 begin
-  VALUES = [1,2,3,4,5,6,7,8,9,10]
+  VALUES = [2,3,4,5,6,7,8,9,"Kings", "Queens", "Jacks", "Ace"]
   SUITS = ["of Hearts", "of Spades", "of Clubs", "of Diamonds"]
     
-  first_player_card = VALUES.product(SUITS).sample
-  second_player_card = VALUES.product(SUITS).sample
-  total = first_player_card[0] + second_player_card[0]
+  first_card = VALUES.product(SUITS).sample
+  second_card = VALUES.product(SUITS).sample
+
+  check_first_player_card = first_card[0].to_i
+  check_second_player_card  = second_card[0].to_i
+
+  first_player_card = check_first_player_card + 10
+  second_player_card = check_second_player_card + 10
+
+  if first_card[0] == "Ace"  
+    check_first_player_card = first_card[0].to_i
+    first_player_card = check_first_player_card + 11
+  elsif second_card[0] == "Ace"
+    check_second_player_card = second_card[0].to_i 
+    second_player_card = check_second_player_card + 11
+  end
+
+  if check_first_player_card == 0 and check_second_player_card == 0
+    total = first_player_card + second_player_card
+        
+  elsif check_first_player_card != 0 and check_second_player_card == 0
+    total = check_first_player_card + second_player_card
+        
+  elsif check_first_player_card == 0 and check_second_player_card != 0
+    total = first_player_card + check_second_player_card
+        
+  elsif check_first_player_card != 0 and check_second_player_card != 0
+    total = check_first_player_card + check_second_player_card
+  end
  
   system "clear"
     
-  print "You got #{first_player_card.join(" ")} and #{second_player_card.join(" ")}. Your total so far is #{total}."
+  print "You got #{first_card.join(" ")} and #{second_card.join(" ")}. Your total so far is #{total}."
   puts
   begin
     puts
     puts "Do you want another card? Yes (y) or No (n):"
     decision = gets.chomp
     extra_card = VALUES.product(SUITS).sample
+    check_extra_card = extra_card[0].to_i
     if decision == "y"
-      new_total = total + extra_card[0]
-      puts
-      puts"Your new card is #{extra_card.join(" ")}. Your new total is #{new_total}."
-      total = new_total 
+      if extra_card[0] == "Ace"
+        check_extra_card = extra_card[0].to_i
+        new_extra_card = check_extra_card + 11
+        new_total = total + new_extra_card
+        if new_total > 21
+          temp_total = new_total - 10
+          new_total = temp_total
+        end
+        puts
+        puts "Your new card is #{extra_card.join(" ")}. Your new total is #{new_total}." 
+        total = new_total
+      elsif check_extra_card == 0
+        new_check_extra_card = check_extra_card + 10
+        new_total = total + new_check_extra_card
+        puts
+        puts "Your new card is #{extra_card.join(" ")}. Your new total is #{new_total}."
+        total = new_total
+      elsif check_extra_card != 0
+        new_total = total + check_extra_card
+        puts
+        puts "Your new card is #{extra_card.join(" ")}. Your new total is #{new_total}." 
+        total = new_total
+      end 
     end
     if total > 21
       puts "Busted!"
@@ -37,21 +83,68 @@ begin
     end
   end until decision == "n" or total == 21 or total > 21
     
-  first_computer_card = VALUES.product(SUITS).sample
-  second_computer_card = VALUES.product(SUITS).sample
-  total_computer = first_computer_card[0] + second_computer_card[0]
+  first_pc_card = VALUES.product(SUITS).sample
+  second_pc_card = VALUES.product(SUITS).sample
+
+  check_first_pc_card = first_pc_card[0].to_i
+  check_second_pc_card  = second_pc_card[0].to_i
+
+  first_computer_card = check_first_pc_card + 10
+  second_computer_card = check_second_pc_card + 10
+
+  if first_pc_card[0] == "Ace"  
+    check_first_pc_player_card = first_pc_card[0].to_i
+    first_computer_card = check_first_player_card + 11
+  elsif second_card[0] == "Ace"
+    check_second_pc_card = second_pc_card[0].to_i 
+    second_computer_card = check_second_pc_card + 11
+  end
+
+  if check_first_pc_card == 0 and check_second_pc_card == 0
+    total_computer = first_computer_card + second_computer_card 
+
+  elsif check_first_pc_card != 0 and check_second_pc_card == 0
+    total_computer = check_first_pc_card + second_computer_card
+        
+  elsif check_first_pc_card == 0 and check_second_pc_card != 0
+    total_computer = first_computer_card + check_second_pc_card
+        
+  elsif check_first_pc_card != 0 and check_second_pc_card != 0
+    total_computer = check_first_pc_card + check_second_pc_card
+  end
 
   puts
-  puts "Computer has #{first_computer_card.join(" ")} and #{second_computer_card.join(" ")}. Total is #{total_computer}."
+  puts "Computer has #{first_pc_card.join(" ")} and #{second_pc_card.join(" ")}. Total is #{total_computer}."
+
   if total_computer < 17 && total <= 21  #Need one when player goes above 21... computer only needs to stay
     begin
       sleep(0.5)
-      computer_extra_card = VALUES.product(SUITS).sample
-      new_computer_total = total_computer + computer_extra_card[0]
+      extra_pc_card = VALUES.product(SUITS).sample
+
+      check_computer_extra_card = extra_pc_card[0].to_i
+      computer_extra_card = check_computer_extra_card + 10
+      
+      if extra_pc_card[0] == "Ace"
+        check_extra_computer_card = extra_pc_card[0].to_i
+        new_extra_pc_card = check_extra_computer_card + 11
+        new_computer_total = total_computer + new_extra_pc_card
+          
+        if new_computer_total > 21
+            temp_pc_total = new_computer_total - 10
+            new_computer_total = temp_pc_total
+        end
+
+      elsif check_computer_extra_card == 0
+        new_computer_total = total_computer + computer_extra_card
+
+      elsif check_computer_extra_card != 0
+        new_computer_total = total_computer + check_computer_extra_card
+      end
+
       total_computer = new_computer_total
       sleep(0.5)
       puts
-      puts "Computer has drawn #{computer_extra_card.join(" ")}. The new computer total is #{total_computer}."  
+      puts "Computer has drawn #{extra_pc_card.join(" ")}. The new computer total is #{total_computer}."  
       sleep(0.5)
     end until total_computer > 21 or total_computer == 21 or (total_computer <= 21 && total_computer >= 17)        
   end
